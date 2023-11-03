1412a4bd6f55349649f9da0f3eedb7dad5b23ea7
Кирилл Новокшонов <novokshonv.kirill@yandex.ru>
2023-11-03 21:31:51 +0500

    Добавьте файлы проекта.

---
+++ SimpleMediaPlayer/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace SimpleMediaPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            videoView1 = new LibVLCSharp.WinForms.VideoView();
+            menuStrip1 = new MenuStrip();
+            файлToolStripMenuItem = new ToolStripMenuItem();
+            открытьToolStripMenuItem = new ToolStripMenuItem();
+            openFileDialog1 = new OpenFileDialog();
+            toolStrip1 = new ToolStrip();
+            toolStripButton1 = new ToolStripButton();
+            toolStripButton2 = new ToolStripButton();
+            toolStripTextBox1 = new ToolStripTextBox();
+            toolStripButton3 = new ToolStripButton();
+            metroTrackBar1 = new MetroFramework.Controls.MetroTrackBar();
+            ((System.ComponentModel.ISupportInitialize)videoView1).BeginInit();
+            menuStrip1.SuspendLayout();
+            toolStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // videoView1
+            // 
+            videoView1.BackColor = Color.Black;
+            videoView1.Dock = DockStyle.Fill;
+            videoView1.Location = new Point(0, 33);
+            videoView1.Margin = new Padding(0);
+            videoView1.MediaPlayer = null;
+            videoView1.Name = "videoView1";
+            videoView1.Size = new Size(800, 450);
+            videoView1.TabIndex = 0;
+            videoView1.Text = "videoView1";
+            videoView1.Click += VideoView1_Click;
+            videoView1.DoubleClick += VideoView1_DoubleClick;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.BackColor = Color.White;
+            menuStrip1.ImageScalingSize = new Size(24, 24);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { файлToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 33);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            файлToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { открытьToolStripMenuItem });
+            файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            файлToolStripMenuItem.Size = new Size(69, 29);
+            файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            открытьToolStripMenuItem.Size = new Size(184, 34);
+            открытьToolStripMenuItem.Text = "Открыть";
+            открытьToolStripMenuItem.Click += открытьToolStripMenuItem_Click;
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.AllowMerge = false;
+            toolStrip1.BackColor = Color.White;
+            toolStrip1.Dock = DockStyle.Bottom;
+            toolStrip1.ImageScalingSize = new Size(24, 24);
+            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripButton1, toolStripButton2, toolStripTextBox1, toolStripButton3 });
+            toolStrip1.Location = new Point(0, 511);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(800, 33);
+            toolStrip1.TabIndex = 2;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            toolStripButton1.BackColor = Color.White;
+            toolStripButton1.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton1.Image = Properties.Resources.play;
+            toolStripButton1.ImageTransparentColor = Color.Magenta;
+            toolStripButton1.Name = "toolStripButton1";
+            toolStripButton1.Size = new Size(34, 28);
+            toolStripButton1.Text = "toolStripButton1";
+            toolStripButton1.Click += toolStripButton1_Click;
+            // 
+            // toolStripButton2
+            // 
+            toolStripButton2.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton2.Image = Properties.Resources.pause;
+            toolStripButton2.ImageTransparentColor = Color.Magenta;
+            toolStripButton2.Name = "toolStripButton2";
+            toolStripButton2.Size = new Size(34, 28);
+            toolStripButton2.Text = "toolStripButton2";
+            toolStripButton2.Click += toolStripButton2_Click;
+            // 
+            // toolStripTextBox1
+            // 
+            toolStripTextBox1.BackColor = Color.White;
+            toolStripTextBox1.Name = "toolStripTextBox1";
+            toolStripTextBox1.ReadOnly = true;
+            toolStripTextBox1.Size = new Size(100, 33);
+            // 
+            // toolStripButton3
+            // 
+            toolStripButton3.Alignment = ToolStripItemAlignment.Right;
+            toolStripButton3.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton3.Image = Properties.Resources.unmute;
+            toolStripButton3.ImageTransparentColor = Color.Magenta;
+            toolStripButton3.Name = "toolStripButton3";
+            toolStripButton3.Size = new Size(34, 28);
+            toolStripButton3.Text = "toolStripButton3";
+            toolStripButton3.Click += toolStripButton3_Click;
+            // 
+            // metroTrackBar1
+            // 
+            metroTrackBar1.BackColor = Color.White;
+            metroTrackBar1.Dock = DockStyle.Bottom;
+            metroTrackBar1.Location = new Point(0, 483);
+            metroTrackBar1.Maximum = 1;
+            metroTrackBar1.Name = "metroTrackBar1";
+            metroTrackBar1.Size = new Size(800, 28);
+            metroTrackBar1.SmallChange = 10;
+            metroTrackBar1.TabIndex = 3;
+            metroTrackBar1.TabStop = false;
+            metroTrackBar1.Text = "metroTrackBar1";
+            metroTrackBar1.Value = 0;
+            metroTrackBar1.Scroll += metroTrackBar1_Scroll;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(144F, 144F);
+            AutoScaleMode = AutoScaleMode.Dpi;
+            ClientSize = new Size(800, 544);
+            Controls.Add(videoView1);
+            Controls.Add(metroTrackBar1);
+            Controls.Add(toolStrip1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            FormClosed += Form1_FormClosed;
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)videoView1).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private LibVLCSharp.WinForms.VideoView videoView1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem файлToolStripMenuItem;
+        private ToolStripMenuItem открытьToolStripMenuItem;
+        private OpenFileDialog openFileDialog1;
+        private ToolStrip toolStrip1;
+        private ToolStripButton toolStripButton1;
+        private ToolStripButton toolStripButton2;
+        private ToolStripTextBox toolStripTextBox1;
+        private MetroFramework.Controls.MetroTrackBar metroTrackBar1;
+        private ToolStripButton toolStripButton3;
+    }
+}
---
+++ SimpleMediaPlayer/Form1.cs
@@ -0,0 +1,193 @@
+using LibVLCSharp.Shared;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace SimpleMediaPlayer
+{
+    public partial class Form1 : Form
+    {
+        public LibVLC _libVLC;
+        public LibVLCSharp.Shared.MediaPlayer _mp;
+        bool clicked;
+        bool fullscreen = false;
+        string path = "";
+        FormWindowState last_state = FormWindowState.Normal;
+        System.Timers.Timer timer = new System.Timers.Timer();
+        public Form1()
+        {
+            if (!DesignMode)
+            {
+                Core.Initialize();
+            }
+            InitializeComponent();
+
+
+
+            _libVLC = new LibVLC();
+            _mp = new LibVLCSharp.Shared.MediaPlayer(_libVLC);
+            videoView1.MediaPlayer = _mp;
+
+            _mp.Playing += OnPlaying;
+            _mp.Paused += OnPaused;
+        }
+
+        #region События формы
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer.Interval = 200;
+            timer.Elapsed += Timer_Elapsed;
+
+            //var media = new Media(_libVLC, new Uri("C:\\Users\\novok\\Downloads\\2.mp4"));
+            //_mp.Play(media);
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            _mp.Stop();
+            _mp.Dispose();
+            _libVLC.Dispose();
+        }
+        #endregion
+
+        #region События плеера
+        private void OnPlaying(object sender, EventArgs e)
+        {
+            timer.Start();
+            int length_s = Convert.ToInt32(_mp.Length / 1000);
+            metroTrackBar1.Maximum = length_s;
+            _mp.EnableKeyInput = false;
+            _mp.EnableMouseInput = false;
+            //toolStripButton1.
+        }
+
+        private void OnPaused(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+        #endregion
+
+        #region События видео
+        private async void VideoView1_Click(object? sender, EventArgs e)
+        {
+            if (clicked) return;
+            clicked = true;
+            await Task.Delay(SystemInformation.DoubleClickTime);
+            if (!clicked) return;
+            clicked = false;
+
+            if (_mp.IsPlaying)
+            {
+                _mp.Pause();
+            }
+            else
+            {
+                _mp.Play();
+            }
+
+        }
+
+        private void VideoView1_DoubleClick(object? sender, EventArgs e)
+        {
+            clicked = false;
+            if (fullscreen)
+            {
+                fullscreen = false;
+
+                menuStrip1.Visible = true;
+                toolStrip1.Visible = true;
+                metroTrackBar1.Visible = true;
+
+                this.FormBorderStyle = FormBorderStyle.Sizable;
+                this.WindowState = last_state;
+            }
+            else
+            {
+                fullscreen = true;
+                last_state = this.WindowState;
+
+                menuStrip1.Visible = false;
+                toolStrip1.Visible = false;
+                metroTrackBar1.Visible = false;
+
+                this.FormBorderStyle = FormBorderStyle.None;
+                this.WindowState = FormWindowState.Normal;
+                this.WindowState = FormWindowState.Maximized;
+            }
+        }
+
+        #endregion
+
+        #region События Menu
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "All Media Files|*.wav;*.aac;*.wma;*.wmv;*.avi;*.mpg;*.mpeg;*.m1v;*.mp2;*.mp3;*.mpa;*.mpe;*.m3u;*.mp4;*.mov;*.3g2;*.3gp2;*.3gp;*.3gpp;*.m4a;*.cda;*.aif;*.aifc;*.aiff;*.mid;*.midi;*.rmi;*.mkv;*.WAV;*.AAC;*.WMA;*.WMV;*.AVI;*.MPG;*.MPEG;*.M1V;*.MP2;*.MP3;*.MPA;*.MPE;*.M3U;*.MP4;*.MOV;*.3G2;*.3GP2;*.3GP;*.3GPP;*.M4A;*.CDA;*.AIF;*.AIFC;*.AIFF;*.MID;*.MIDI;*.RMI;*.MKV,*.webm";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                path = openFileDialog1.FileName;
+                var media = new Media(_libVLC, new Uri(path));
+                _mp.Play(media);
+            }
+        }
+        #endregion
+
+        #region Tool
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            _mp.Play();
+        }
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            _mp.Pause();
+        }
+        #endregion
+
+        private void Timer_Elapsed(object? sender, ElapsedEventArgs e)
+        {
+            TimeSpan t = TimeSpan.FromMilliseconds(_mp.Time);
+            int time_s = Convert.ToInt32(_mp.Time / 1000);
+
+            if (toolStripTextBox1.Control.InvokeRequired)
+            {
+                toolStripTextBox1.Control.Invoke((MethodInvoker)delegate
+                {
+                    toolStripTextBox1.Text = t.ToString(@"hh\:mm\:ss");
+                });
+            }
+            else
+            {
+                toolStripTextBox1.Text = t.ToString(@"hh\:mm\:ss");
+            }
+
+            if (metroTrackBar1.InvokeRequired)
+            {
+                metroTrackBar1.Invoke((MethodInvoker)delegate
+                {
+                    metroTrackBar1.Value = time_s;
+                });
+            }
+            else
+            {
+                metroTrackBar1.Value = time_s;
+            }
+        }
+
+        private void metroTrackBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            _mp.Time = metroTrackBar1.Value * 1000;
+            TimeSpan t = TimeSpan.FromMilliseconds(_mp.Time);
+            toolStripTextBox1.Text = t.ToString(@"hh\:mm\:ss");
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            if (_mp.Mute)
+            {
+                toolStripButton3.Image = Properties.Resources.unmute;
+            }
+            else
+            {
+                toolStripButton3.Image = Properties.Resources.mute;
+            }
+            _mp.ToggleMute();
+        }
+    }
+}
